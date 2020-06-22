49ab2823a2283a469cd244557bc73c1d637e9a4a
jainsid12 <200coolsid12@gmail.com>
2020-06-22 16:36:45 +0530

    Updates

---
+++ precache-manifest.fd0fd0a22d217fd340f1851872a79e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98413e4ae954fa1ce39cbd0cbf77bcc",
+    "url": "/light-out-game/index.html"
+  },
+  {
+    "revision": "f772449d1800a407ca73",
+    "url": "/light-out-game/static/css/main.e4719928.chunk.css"
+  },
+  {
+    "revision": "d551bd42921c7153df0d",
+    "url": "/light-out-game/static/js/2.eb18e330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/light-out-game/static/js/2.eb18e330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f772449d1800a407ca73",
+    "url": "/light-out-game/static/js/main.7f477791.chunk.js"
+  },
+  {
+    "revision": "86eba01cef3f0005608b",
+    "url": "/light-out-game/static/js/runtime-main.47b911d6.js"
+  }
+]);
